c5e88ca928eb651d1e82447cdcd2a2ec6c05cfa0
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2016-08-27 11:46:51 -0400

    Navbar fix
    Fix navbar functionality. Begin carousel work.

--- js/project.js
+++ js/project.js
@@ -1,3 +1,11 @@
-document.getElementById("mobile").onclick = function(){
-  document.querySelector("nav").classList.toggle("open");
+document.getElementById("mobile").onclick = function() {
+    document.querySelector(".small").classList.toggle("open");
 };
+
+$("#carousel a").click(function(){
+  $("#carousel a.current").removeClass("slide current");
+  $(this).addClass("slide current");
+  var whichSlide = $(this).index();
+  $("div.current").removeClass("current");
+  $("div").eq(whichSlide).addClass("current");
+});
