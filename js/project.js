f2dc60e5d8b9bdc64f3b8cc3cd1834c384039978
Katelyn Mirabelli <katelyn.mirabelli@gmail.com>
2016-08-29 21:03:14 -0400

    Updating gh-pages from master branch via GitBot

--- js/project.js
+++ js/project.js
@@ -1,14 +1,11 @@
-$(document).ready()
-
 document.getElementById("mobile").onclick = function() {
     document.querySelector(".small").classList.toggle("open");
 };
 
-$(".carousel a").click(function(){
-  $(".carousel a.current").removeClass("current");
-  $(this).addClass("current");
-  var whichSlide = $(this).index();
-  $(".slide.current").removeClass("current");
-  $(".slide").eq(whichSlide).addClass("current");
+$(".carousel a").click(function() {
+    $(".carousel a.current").removeClass("current");
+    $(this).addClass("current");
+    var whichSlide = $(this).index();
+    $(".slide.current").removeClass("current");
+    $(".slide").eq(whichSlide).addClass("current");
 });
-
